c75618fdd1b1699438e3fc2aa1f1ee48cc9e74ad
satvinderpalsingh <satvinderpalsingh52663@gmail.com>
2024-12-25 00:11:09 +0530

    added code for decorator design pattern

--- src/designPatterns/decoratorDesignPattern/toppings/ExtraCheeseToppingPizza.java
+++ src/designPatterns/decoratorDesignPattern/toppings/ExtraCheeseToppingPizza.java
@@ -1,7 +1,9 @@
 package designPatterns.decoratorDesignPattern.toppings;
 
 import designPatterns.decoratorDesignPattern.BasePizza;
-
+//Both has-a relationship and is-a relationship are present in the code.
+//has-a relationship is present because ExtraCheeseToppingPizza has a BasePizza.
+//is-a relationship is present because ExtraCheeseToppingPizza is a ToppingDecorator.
 public class ExtraCheeseToppingPizza extends ToppingDecorator {
     private BasePizza basePizza;
 
